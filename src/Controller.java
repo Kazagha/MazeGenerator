ca03dc98237758f7d752f3b24f0d1fd0c7ce9f50
Kazagha <chrisg.local@gmail.com>
2014-11-07 12:47:18 +1000

    Controller: Add new actions to show/hide the maze, load and remove walls.

--- src/Controller.java
+++ src/Controller.java
@@ -10,19 +10,31 @@ public class Controller {
 	private Model model;
 	//private String[] mazeType = new String[] {"Grid"};
 
-	public Controller(View v)
+	public Controller(Model m, View v)
 	{
 		this.view = v;
+		this.model = m;
+		
+		v.setModel(m);
+				
 		v.setActionListener(new MyActionListener());
-		v.setMazeOptions(new String[] {" ", "Load Grid", "Do Something"});
+		
+		v.setMazeOptions(new String[] 
+			{	" ",
+				"Load Model",
+				"Reset Model",
+				"Show Model",
+				"Hide Model",
+				"Do Something"
+			});
 	}
 	
 	public void doChanges()
 	{
 		for(int i = 0; i < model.get_Y_Height(); i++)
 		{
-		model.getNode(2, i).setNorth(false);
-		model.getNode(2, i).setSouth(false);
+			model.getNode(2, i).setNorth(false);
+			model.getNode(2, i).setSouth(false);
 		}
 	}
 	
@@ -37,18 +49,28 @@ public class Controller {
 							
 				switch((String) combo.getSelectedItem())
 				{
-				case "Load Grid":
+				case "Load Model":
 					model = new Model(5, 10);
 					model.setWalls(true);
-	
-					view.setNodeModel(model);
-					view.showMazeViewer();
-					
-					view.revalidate();
-					System.out.println("action running");				
+					view.setModel(model);
+					view.modelUpdated();
+					break;
+				case "Reset Model":
+					model.setWalls(true);
+					view.modelUpdated();
+					break;
+				case "Show Model":	
+					//view.showMazeViewer(false);
+					view.setMazeViewerVisible(true);
+					//view.revalidate();
+					//view.modelUpdated();
+					break;
+				case "Hide Model":
+					view.setMazeViewerVisible(false);
 					break;
 				case "Do Something":
-					System.out.println("Do Something");
+					doChanges();
+					view.modelUpdated();
 					break;
 				}
 			} else if (e.getSource() instanceof JButton) {
